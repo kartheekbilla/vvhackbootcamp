6aad82038a11b72e964a12c8e8342afc33141d14
kartheekbilla <kartheekbilla147@gmail.com>
2024-08-03 16:55:12 +0530

    hey

---
+++ javascript/variables.js
@@ -0,0 +1,54 @@
+// let obj=[1,2,3,4,5]
+// console.log(typeof(obj))
+let hello={
+     //name:"Kartheek",
+    reg:"23B91A6118"
+    }
+// console.log(hello.name)
+
+// console.log(hello.name&&"Data found")
+// console.log(hello.name||"Data not found")
+// console.log(hello.name ? hello.reg : "Data not found")
+
+// if(hello.name)
+// {
+//     console.log(hello.reg);
+// }
+// else(hello.reg) {
+//     console.log(hello.name);
+// }
+
+//console.log(!!(hello.name))
+
+// let num=2
+// switch(num)
+// {
+//     case 1: console.log(hello.name)
+//             break;
+//     case 2: console.log(hello.reg)
+//             break;
+//     default: console.log("NO DATA")
+// }
+
+let arr=[10,20,30,40,50]
+// for(i=0;i<5;i++)
+// {
+//     console.log(arr[i])
+// }
+
+//printing elements
+// for(let x of arr){
+//     console.log(x)
+// } 
+
+// //printing index number
+// for(let x in arr){
+//     console.log(x)
+// }
+
+let i=0
+while(i<5)
+{
+    console.log(i)
+    i++
+}
